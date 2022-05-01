9599dce7fe73859fff423662019f9ab456b28372
supriya kumari <supriyasupriya5645@gmail.com>
2022-05-01 11:45:47 +0530

    form-projrct completed

---
+++ src/Registration.js
@@ -0,0 +1,20 @@
+import React from 'react';
+function App() {
+    return (
+      <form>
+      <label>First Name</label>
+      <input type="text"/>
+      <label>Last Name</label>
+      <input type="text"/>
+      <label>Email</label>
+      <input type="email"/>
+      <label>Passowrd</label>
+      <input type="password"/>
+      <button>Submit</button>
+      </form>
+     
+    );
+  }
+  
+  export default App;
+  
